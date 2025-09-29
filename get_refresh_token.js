e98dba7926de5099f14ffe69c59cc64f6927fb1e
rebmichelvishedsky <rebmichelvishedsky@gmail.com>
2025-09-28 22:07:59 -0400

    Create get_refresh_token.js

---
+++ get_refresh_token.js
@@ -0,0 +1,24 @@
+const { google } = require("googleapis");
+const readline = require("readline");
+
+const oauth2Client = new google.auth.OAuth2(
+  "YOUR_CLIENT_ID",
+  "YOUR_CLIENT_SECRET",
+  "YOUR_REDIRECT_URI"
+);
+
+const SCOPES = ["https://www.googleapis.com/auth/youtube.upload"];
+
+const authUrl = oauth2Client.generateAuthUrl({
+  access_type: "offline",
+  scope: SCOPES,
+});
+
+console.log("Authorize this app by visiting:", authUrl);
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+rl.question("Enter the code from that page here: ", async (code) => {
+  const { tokens } = await oauth2Client.getToken(code);
+  console.log("Refresh Token:", tokens.refresh_token);
+  rl.close();
+});
